c77e5ec256eeb57febb1b0e573d652b2d2625fae
cwz920716 <cwz920716@gmail.com>
2015-11-11 15:01:28 -0600

    checkout redis updates

--- redis-server/server.js
+++ redis-server/server.js
@@ -11,16 +11,21 @@ var http = require('http'), server,
     redis_client = require('redis').createClient();
 
 server = http.createServer(function (request, response) {
-    var beforeTime = process.hrtime();
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
     var redis_info, total_requests;
 
-    var r = Math.random();
+    redis_client.info(function (err, reply) {
+        redis_info = reply; // stash response in outer scope
+    });
+    redis_client.incr('requests', function (err, reply) {
+        total_requests = reply; // stash response in outer scope
+    });
+    redis_client.hincrby('ip', request.connection.remoteAddress, 1);
     redis_client.hgetall('ip', function (err, reply) {
-
         // This is the last reply, so all of the previous replies must have completed already
-        response.writeHead(200, {
-          'Content-Type': 'text/plain'
-        });
         response.write('This page was generated after talking to redis.\n\n' +
             'Redis info:\n' + redis_info + '\n' +
             'Total requests: ' + total_requests + '\n\n' +
@@ -28,19 +33,6 @@ server = http.createServer(function (request, response) {
         Object.keys(reply).forEach(function (ip) {
             response.write('    ' + ip + ': ' + reply[ip] + '\n');
         });
-
-      var afterTime = process.hrtime();
-      var diff2 = diff(beforeTime, afterTime);
-      total += diff2;
-      if (diff2 > maxTime) maxTime = diff2;
-      n++;
-      if (diff(lastp, afterTime) > 1e9 * 10) {
-        console.log('avg redis IO = ' + (total / n) + ' n = ' + n);
-        console.log('max redis IO = ' + maxTime + ' n = ' + n);
-        maxTime = 0;
-        lastp = afterTime;
-        
-      }
         response.end();
     });
 }).listen(50000);
