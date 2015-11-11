02876b950eda2fabdbc527982ae2c72dd0b14729
joshrandall <jrandall@utexas.edu>
2015-11-11 15:16:15 -0600

    checkin w/ cleaner cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,8 +2,16 @@
 
 cd node-v4.2.1
 make clean
-
 cd ..
+
+cd redis-server
+sudo rm -f *.txt
+cd ..
+
+cd tools
+sudo rm -f *.txt *.cdf *.pdf *.plt
+cd ..
+
 for file in `find . -type f -name '*~'`
 do
   echo $file
