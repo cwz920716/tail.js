fe9b01e40e753b814fd6a1586115c38236dda66b
joshrandall <jrandall@utexas.edu>
2016-02-02 20:12:15 -0600

    prepare to migrate

--- lighter/_run.sh
+++ lighter/_run.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 bench=lighter
-cli=cwz@146.6.53.187
+cli=cwz@146.6.53.154
 node_bin_path=node
 
 f=0
@@ -9,6 +9,6 @@ f=0
 $node_bin_path server.js 146.6.53.156 50000 &
 sleep 10
 # ssh -p 2002 $cli '~/dev-tools/wrk2/wrk -R1000 -t200 -c200 -d90s -s ~/dev-tools/wrk2/scripts/lighter.lua http://146.6.53.156:50000/'
-ssh -p 2002 $cli '~/dev-tools/wrk2/wrk -R400 -t50 -c50 -d90s http://146.6.53.156:50000/logo.png'
+ssh -p 2002 $cli '~/dev-tools/wrk2/wrk -R600 -t100 -c100 -d90s -s ~/dev-tools/wrk2/scripts/lighter.lua http://146.6.53.156:50000/'
 pkill -9 node
 sleep 10
